f632aa3c3fdf33286656e2b9994630d5194506e0
bumbleblo <bumbleblo2013@gmail.com>
2017-08-20 23:30:41 -0300

    update organização

---
+++ visulizacao_dados/tsne.py
@@ -0,0 +1,33 @@
+#visalizando um dado muiltidimensional com o T-SNE
+import sklearn.manifold as sk
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('result.csv')
+sv = np.asarray(data['Tipo'])
+#retirnado dado do DataFrame
+del data['Unnamed: 0'] # naõ sei o porque mas ele está carregando lixo no arquivo
+del (data['Tipo'])
+
+
+tsne = sk.TSNE(n_components=2, random_state=0)
+
+teste1 = tsne.fit_transform(data)
+
+color={3:'red', 1:'yellow', 2:'green'}
+marcadores={3:'s',2:'d',1:'p'}
+plt.figure()
+
+plt.title("Visualização dos dados por T-SNE")
+
+for cl in range(0,150):
+		cor = color[sv[cl]]
+		mark = marcadores[sv[cl]]
+		plt.scatter(teste1[cl,0],teste1[cl,1],c=cor,marker=mark,s=40)
+
+plt.xlim([-350,350])
+plt.ylim([-350,350])
+plt.legend(['Bacteria 1', 'Bacteria 2','Bacteria 3'],loc='upper left')
+plt.show()
+plt.savefig('tsne.png')
